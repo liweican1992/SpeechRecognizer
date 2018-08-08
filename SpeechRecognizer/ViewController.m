149e5f99c8eddaf8a3b37804047041a3b95afe9d
liwc <liwc@rd.netease.com>
2018-08-08 22:45:01 +0800

    Initial Commit

---
+++ SpeechRecognizer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SpeechRecognizer
+//
+//  Created by 李伟灿 on 2018/8/8.
+//  Copyright © 2018年 CC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
